5c0f09d789203a7ceef1b89021371c9a2ba91534
MO7AMMED-ISMAIL <mo7medismail200@gmail.com>
2024-05-01 23:16:39 +0300

    make resource

---
+++ backend/app/Http/Resources/ReportProductResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ReportProductResource extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'customer_id' => $this->customer_id,
+            'product_id' => $this->product_id,
+            'reason' => $this->reason,
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+            'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
+        ];
+    }
+}
