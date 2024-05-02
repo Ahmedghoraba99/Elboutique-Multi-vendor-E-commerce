1b691ed23d33bcced33e5cf1853b82e22ecf969c
MO7AMMED-ISMAIL <mo7medismail200@gmail.com>
2024-05-02 22:32:54 +0300

    Create and show Reviews

--- backend/app/Http/Requests/StoreReviewRequest.php
+++ backend/app/Http/Requests/StoreReviewRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class StoreReviewRequest extends FormRequest
 {
@@ -11,7 +13,7 @@ class StoreReviewRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +24,34 @@ class StoreReviewRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'rate' => 'required|integer|min:1|max:5',
+            'customer_id' => 'required|exists:customers,id',
+            'product_id' => 'required|exists:products,id',
         ];
+    }
+
+
+        /**
+     * Get the validation error messages.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'rate.required' => 'The rate field is required.',
+            'rate.integer' => 'The rate field must be an integer.',
+            'rate.min' => 'The rate field must be at least :min.',
+            'rate.max' => 'The rate field may not be greater than :max.',
+            'customer_id.required' => 'The customer ID field is required.',
+            'customer_id.exists' => 'The selected customer ID is invalid.',
+            'product_id.required' => 'The product ID field is required.',
+            'product_id.exists' => 'The selected product ID is invalid.',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json($validator->errors(), 422));
     }
 }
--- backend/app/Models/Review.php
+++ backend/app/Models/Review.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'customer_id',
+        'product_id',
+        'rate',
+    ];
+
 }
