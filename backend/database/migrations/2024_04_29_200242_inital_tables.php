ae94fb42b1dcd3655a42147aea246a05ee3eb020
Ahmedghoraba99 <ahmed.12.2011@gmail.com>
2024-05-03 02:47:38 +0300

    -FIXING initial table

--- backend/database/migrations/2024_04_29_200242_inital_tables.php
+++ backend/database/migrations/2024_04_29_200242_inital_tables.php
@@ -36,21 +36,21 @@ return new class extends Migration
         // end of user related tables
 
         // start of product tables
-        Schema::create('products', function (Blueprint $table) {
-            $table->id();
-        });
-        Schema::create('categories', function (Blueprint $table) {
-            $table->id();
-        });
-        Schema::create('tags', function (Blueprint $table) {
-            $table->id();
-        });
-        Schema::create('product_attributes', function (Blueprint $table) {
-            $table->id();
-        });
-        Schema::create('product_images', function (Blueprint $table) {
-            $table->id();
-        });
+        // Schema::create('products', function (Blueprint $table) {
+        //     $table->id();
+        // });
+        // Schema::create('categories', function (Blueprint $table) {
+        //     $table->id();
+        // });
+        // Schema::create('tags', function (Blueprint $table) {
+        //     $table->id();
+        // });
+        // Schema::create('product_attributes', function (Blueprint $table) {
+        //     $table->id();
+        // });
+        // Schema::create('product_images', function (Blueprint $table) {
+        //     $table->id();
+        // });
 
         // end of product tables
 
@@ -84,6 +84,22 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('customers');
+        Schema::dropIfExists('address');
+        Schema::dropIfExists('customer_address');
+        Schema::dropIfExists('vendor_address');
+        Schema::dropIfExists('customer_phone');
+        Schema::dropIfExists('vendor_phone');
+        Schema::dropIfExists('reviews');
+        Schema::dropIfExists('products');
+        Schema::dropIfExists('categories');
+        Schema::dropIfExists('tags');
+        Schema::dropIfExists('product_report');
+        Schema::dropIfExists('review_report');
+        Schema::dropIfExists('product_attributes');
+        Schema::dropIfExists('product_images');
+        Schema::dropIfExists('whishlist');
+        Schema::dropIfExists('cart');
+        Schema::dropIfExists('payments');
     }
 };
