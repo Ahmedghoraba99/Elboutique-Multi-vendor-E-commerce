513d9c9e0ac46d6ec7ce4f8f79fc1a48d7bfe55b
Ahmedghoraba99 <ahmed.12.2011@gmail.com>
2024-05-03 02:41:25 +0300

    -Tags model

---
+++ backend/app/Http/Controllers/TagController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTagRequest;
+use App\Http\Requests\UpdateTagRequest;
+use App\Models\Tag;
+
+class TagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tags = Tag::paginate(10);
+        if (!$tags->count()) {
+            return response()->json([
+                'message' => 'No tags found'
+            ], 404);
+        }
+        return response()->json($tags);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTagRequest $request)
+    {
+        try {
+            $tag = Tag::create($request->validated());
+            return response()->json($tag, 201);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Error creating tag', 'error' => $th->getMessage()
+            ], 500);
+        };
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(int $tag)
+    {
+        $tag = Tag::find($tag);
+        if (!$tag) {
+            return response()->json([
+                'message' => 'Tag not found'
+            ], 404);
+        }
+        return response()->json($tag);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTagRequest $request, Tag $tag)
+    {
+        $tag = Tag::find($tag);
+        if (!$tag) {
+            return response()->json([
+                'message' => 'Tag not found'
+            ], 404);
+        }
+        $tag->update($request->validated());
+        return response()->json($tag);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(int $tag)
+    {
+        $tag = Tag::find($tag);
+        if (!$tag) {
+            return response()->json([
+                'message' => 'Tag not found'
+            ], 404);
+        }
+        $tag->delete();
+        return response()->json([
+            'message' => 'Tag deleted'
+        ], 200);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,32 +1,31 @@
 <?php
+
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductImagesController;
 use App\Http\Controllers\OrderProductController;
 use App\Http\Controllers\VendorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::group([],function(){
-Route::apiResource('admins',AdminController::class);
-Route::apiResource('vendors',VendorController::class);
+
+// API routes
+Route::group([], function () {
+    Route::apiResource('admins', AdminController::class);
+    Route::apiResource('vendors', VendorController::class);
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('tags', TagController::class);
+    Route::apiResource('products', ProductController::class);
 });
-
-
+// Order related routes
+Route::post('/orders/{id}/products', [OrderProductController::class, 'addProductToOrder']);
 Route::apiResource('orders', OrderController::class);
-
-Route::post('/orders/{id}/products',[OrderProductController::class,'addProductToOrder']);
-Route::get('/orders/{id}/products',[OrderProductController::class,'getOrderProduct']);
+Route::post('/orders/{id}/products', [OrderProductController::class, 'addProductToOrder']);
+Route::get('/orders/{id}/products', [OrderProductController::class, 'getOrderProduct']);
