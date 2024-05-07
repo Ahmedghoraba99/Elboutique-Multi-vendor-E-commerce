0486d01bb951b5a602665018e65222f84f63c7b4
Your Name <m.abdelazem0202@gmail.com>
2024-05-08 00:34:38 +0300

    update routes

--- elboutique/src/app/app.routes.ts
+++ elboutique/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Elboutique',
+    component: HomeComponent,
+  },
+  {
+    path: 'dashboard',
+    title: 'Dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: '**',
+    title: 'Page not found',
+    component: PageNotFoundComponent,
+  },
+];
