1ce9b699e19f54bee04b7ebea57a82686bb263d0
Your Name <m.abdelazem0202@gmail.com>
2024-06-18 05:03:27 +0300

    register screen

--- elboutique/src/app/app.routes.ts
+++ elboutique/src/app/app.routes.ts
@@ -3,7 +3,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './Auth/login/login.component';
-
+import { ForgotPasswordComponent } from './Auth/forget-password/forget-password.component';
+import { RegisterComponent } from './Auth/register/register.component';
 export const routes: Routes = [
   {
     path: '',
@@ -19,6 +20,16 @@ export const routes: Routes = [
     path: 'login',
     title: 'Login',
     component: LoginComponent,
+  },
+  {
+    path: 'login/forget-password',
+    title: 'ForgetPassword',
+    component: ForgotPasswordComponent,
+  },
+  {
+    path: 'register',
+    title: 'Register',
+    component: RegisterComponent,
   },
   {
     path: '**',
