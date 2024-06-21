47854a0f41237c1b0480019ac040482ac24a7ed6
Ahmedghoraba99 <ahmed.12.2011@gmail.com>
2024-06-21 13:02:04 +0300

    (feat) dynamic big deals

---
+++ elboutique/src/app/service/home.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root',
+})
+export class HomeService {
+  httpClient = inject(HttpClient);
+  private baseUrl = 'http://localhost:8000/api';
+
+  getFeaturedProducts(): Observable<any> {
+    return this.httpClient
+      .get<any>(`${this.baseUrl}/product/featured`)
+      .pipe(tap((data) => console.log('All: ', JSON.stringify(data))));
+  }
+
+  getOnSaleProducts(): Observable<any> {
+    return this.httpClient
+      .get<any>(`${this.baseUrl}/product/onsale`)
+      .pipe(tap((data) => console.log('All: ', JSON.stringify(data))));
+  }
+
+  getNewArrivalsProducts(): Observable<any> {
+    return this.httpClient
+      .get<any>(`${this.baseUrl}/product/newarrivals`)
+      .pipe(tap((data) => console.log('All: ', JSON.stringify(data))));
+  }
+
+  constructor() {}
+}
