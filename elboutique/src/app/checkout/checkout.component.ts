6b5ff5366e5dfffe7df958f950af06959615831e
sasuna44 <saramagdi44@std.mans.edu.eg>
2024-06-22 22:07:04 +0300

    fix:buttons

--- elboutique/src/app/checkout/checkout.component.ts
+++ elboutique/src/app/checkout/checkout.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { SummaryOrderComponent } from './summary-order/summary-order.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {  faHeart , faTrashCan  } from '@fortawesome/free-regular-svg-icons';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [SummaryOrderComponent],
+  imports: [SummaryOrderComponent ,FontAwesomeModule ,CommonModule],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 
 })
-export class CheckoutComponent {
-
+export class CheckoutComponent  {
+  faHeart = faHeart;
+  faTrashCan=faTrashCan;
+  addToWishlist() {
+   
+  }
+  DeleteFromWishlist(){}
 }
