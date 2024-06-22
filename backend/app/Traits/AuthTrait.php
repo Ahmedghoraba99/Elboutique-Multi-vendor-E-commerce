42a890c15378396a5ceaed0924266e3948b8565c
dabour1 <Aldabor63@Yahoo.Com>
2024-06-22 23:38:09 +0300

    some edit for  req validations

--- backend/app/Traits/AuthTrait.php
+++ backend/app/Traits/AuthTrait.php
@@ -20,61 +20,8 @@ trait AuthTrait {
         }
         return   $imageName ;
     }
-    // public function reqValidation($user='', $priority='required',$user_id='' ){
-        
-    //     return array_merge(
-    //         $this->commenRules($priority),
-    //         $this->email_phone_rules($user,'email',$priority,$user_id),
-    //         $this->email_phone_rules($user,'phone',$priority,$user_id),
-    //     );
-        
-        
-    // }
-    // private function commenRules($priority): array{
-    //     return [
-             
-    //         'name' => "$priority|string|max:255|min:4|regex:/^[a-zA-Z ,.\'-]+$/",
-    //         'password' => "$priority|string|min:8|regex:/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/",
-    //         'address' => 'sometimes|string|max:255|min:10',
-    //         'image' => 'sometimes|nullable|image|mimes:jpg,png|max:2048',
-    //     ];
 
-    // }
-
-    // private function email_phone_rules($user,$field,$priority='required',$user_id): array
-    // {
-       
-    //     if($field=="email"){
-    //         $regex = '/^[^@\s]+@[^@\s]+\.[^@\s]+$/';
-    //     }
-    //     else if($field=="phone"){
-    //         $regex = '/^01[012]\d{8}$/';
-    //     }
-    //     $validationArray=[
-    //         "$field" => [
-    //             "$priority",
-    //             'string',
-    //             'max:50',
-    //             "regex:$regex",
-    //         ],
-    //     ];
-    //     if($user=='admin'&&$priority=='sometimes'){
-            
-    //         array_push($validationArray["$field"],Rule::unique('admins')->ignore($user_id),
-    //         Rule::unique('users'));
-    //     }
-    //     elseif($user=='user'&&$priority=='sometimes'){
-        
-    //         array_push($validationArray["$field"],Rule::unique('users')->ignore($user_id),
-    //         Rule::unique('admins'));
-    //     }else{
-            
-    //         array_push($validationArray["$field"],Rule::unique('admins'),
-    //         Rule::unique('users'));
-    //     }
-       
-    //     return $validationArray;
-    // }
+   
 
    
 
