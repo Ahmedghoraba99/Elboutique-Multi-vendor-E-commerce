5e5f58b7bf66f575fee83f4bd637d61ee12690ba
sasuna44 <saramagdi44@std.mans.edu.eg>
2024-06-24 00:33:23 +0300

    addNavbar

--- elboutique/src/app/app.component.ts
+++ elboutique/src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
+import { NavbarComponent } from './layout/navbar/navbar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FontAwesomeModule, RouterLink],
+  imports: [NavbarComponent, RouterOutlet, FontAwesomeModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
