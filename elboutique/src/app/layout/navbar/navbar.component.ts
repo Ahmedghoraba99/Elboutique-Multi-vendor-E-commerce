a244438036e41ce8499b10fcee35beefd9bf616d
Ahmedghoraba99 <ahmed.12.2011@gmail.com>
2024-06-25 16:58:15 +0300

    (upgrade) linking parts of nav and footer

--- elboutique/src/app/layout/navbar/navbar.component.ts
+++ elboutique/src/app/layout/navbar/navbar.component.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
 import { SelectDropComponent } from './select-drop/select-drop.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faSearch , faShoppingCart ,faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons';
-import { faHeart  } from '@fortawesome/free-regular-svg-icons';
+import {
+  faSearch,
+  faShoppingCart,
+  faArrowRightFromBracket,
+} from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [FontAwesomeModule ,SelectDropComponent] ,
+  imports: [FontAwesomeModule, RouterLink, SelectDropComponent],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-  faSearch=faSearch;
-  faShoppingCart=faShoppingCart;
-  faHeart=faHeart;
-  faArrowRightFromBracket=faArrowRightFromBracket;
+  faSearch = faSearch;
+  faShoppingCart = faShoppingCart;
+  faHeart = faHeart;
+  faArrowRightFromBracket = faArrowRightFromBracket;
 }
