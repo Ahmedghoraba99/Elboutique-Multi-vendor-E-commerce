a61156275dd60ea1933ae4e3c2a6c3a9e2b40992
dabour1 <Aldabor63@Yahoo.Com>
2024-06-27 02:50:19 +0300

    Unauthorization using guards (AdminVendorAuth Middleware)

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'AdminCustomerAuth' => \App\Http\Middleware\AdminCustomerAuth::class,
+        'AdminVendorAuth' => \App\Http\Middleware\AdminVendorAuth::class,
     ];
 }
