080e00f56ea3572b20c7d4c54fcd242aa22162d0
dabour1 <Aldabor63@Yahoo.Com>
2024-06-27 04:12:54 +0300

    fix TokenInterceptor error

--- elboutique/src/app/_interceptor/token.interceptor.ts
+++ elboutique/src/app/_interceptor/token.interceptor.ts
@@ -2,7 +2,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const TokenInterceptor: HttpInterceptorFn = (req, next) => {
   const storageData = localStorage.getItem('user_info');
-  const token = JSON.parse(storageData!).token;
+  const token = JSON.parse(storageData!)?.token;
 
   if (token) {
     req = req.clone({
