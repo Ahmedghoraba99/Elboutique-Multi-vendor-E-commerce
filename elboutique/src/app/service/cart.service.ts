114d39310f615a4be443ed6bec2945d091ef352e
Ahmedghoraba99 <ahmed.12.2011@gmail.com>
2024-06-27 17:09:51 +0300

    (feat) -Added Observers to services

--- elboutique/src/app/service/cart.service.ts
+++ elboutique/src/app/service/cart.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject, switchMap, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,8 +9,14 @@ export class CartService {
   private baseUrl = 'http://127.0.0.1:8000/api/customer';
   private userInfo = localStorage.getItem('user_info');
   private userID = this.userInfo ? JSON.parse(this.userInfo).id : null;
+  private cart = new BehaviorSubject<any>(null);
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    // Fetch initial cart data when service is instantiated
+    if (this.userID) {
+      this.fetchCustomerCart();
+    }
+  }
 
   getCustomerCart(): Observable<any> {
     return this.http.get(`${this.baseUrl}/showCart/${this.userID}`);
@@ -20,5 +26,31 @@ export class CartService {
   }
   deleteFromCustomerCart(body: Object): Observable<any> {
     return this.http.post(`${this.baseUrl}/deleteCart/${this.userID}`, body);
+  }
+  // ************
+  fetchCustomerCart(): void {
+    this.getCustomerCart().subscribe((cartData) => {
+      this.cart.next(cartData);
+    });
+  }
+  getCartData(): Observable<any> {
+    return this.cart.asObservable();
+  }
+  addItemToCart(body: Object): void {
+    this.addToCustomerCart(body)
+      .pipe(
+        switchMap(() => this.getCustomerCart()),
+        tap((cartData) => this.cart.next(cartData))
+      )
+      .subscribe();
+  }
+
+  deleteItemFromCart(body: Object): void {
+    this.deleteFromCustomerCart(body)
+      .pipe(
+        switchMap(() => this.getCustomerCart()),
+        tap((cartData) => this.cart.next(cartData))
+      )
+      .subscribe();
   }
 }
--- elboutique/src/app/service/wishlist.service.ts
+++ elboutique/src/app/service/wishlist.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,10 +9,14 @@ export class WishlistService {
   private baseUrl = 'http://127.0.0.1:8000/api/customer';
   private userInfo = localStorage.getItem('user_info');
   private userID = this.userInfo ? JSON.parse(this.userInfo).id : null;
-  
+
+  private wishlist = new BehaviorSubject<any>(null);
+
   constructor(private http: HttpClient) {}
 
   getUserWishlist(): Observable<any> {
+    console.log(this.userID);
+
     return this.http.get(`${this.baseUrl}/showWishlist/${this.userID}`);
   }
   addToUserWishlist(body: Object): Observable<any> {
@@ -23,5 +27,28 @@ export class WishlistService {
       `${this.baseUrl}/deleteWishlist/${this.userID}`,
       body
     );
+  }
+
+  // **********
+  // Methods with observrs
+  fetchUserWishlist(): void {
+    this.getUserWishlist().subscribe((wishlistData) => {
+      this.wishlist.next(wishlistData);
+    });
+  }
+  getWishlistData(): Observable<any> {
+    this.fetchUserWishlist();
+    return this.wishlist.asObservable();
+  }
+  addItemToWishlist(body: Object): void {
+    this.addToUserWishlist(body).subscribe(() => {
+      this.fetchUserWishlist();
+    });
+  }
+
+  deleteItemFromWishlist(body: Object): void {
+    this.deleteFromUserWishlist(body).subscribe(() => {
+      this.fetchUserWishlist();
+    });
   }
 }
