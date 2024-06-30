467b189f328a89bb73e555f323678fd6d744ba27
MO7AMMED-ISMAIL <mo7medismail200@gmail.com>
2024-07-01 01:06:15 +0300

    add VendorReceivables to vender

--- elboutique/src/app/profile/vendor/overview/overview.component.ts
+++ elboutique/src/app/profile/vendor/overview/overview.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {VendorReceivables} from '../../../_model/vendor-receivables';
+import { ReceivablesService } from '../../../service/receivables.service';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-vendor-overview',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.css',
 })
-export class OverviewComponent {}
+export class OverviewComponent implements OnInit{
+
+  vendorReceivables: VendorReceivables | null = null;
+  errorMessage: string = '';
+  customer_id: number = 0;
+
+
+  constructor(private amountServise:ReceivablesService){
+    this.customer_id = JSON.parse(localStorage.getItem('user_info') || '{}').id
+  }
+  ngOnInit(): void {
+    this.loadVendorReceivables();
+  }
+
+  loadVendorReceivables() {
+    this.amountServise.getVendorReceivables(this.customer_id).subscribe(
+      (response) => {
+        this.vendorReceivables = response;
+      },
+      (error) => {
+        this.errorMessage = error.errorMessage;
+      }
+    );
+  }
+
+
+
+}
